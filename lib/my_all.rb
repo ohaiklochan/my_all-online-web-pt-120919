27bd03dd71439a9da161b1b435508856c0fe13a2
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 00:10:32 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,5 +8,9 @@ collection.length
     block_return_values << yield(collection[i])
     i = i + 1
   end
-  if block_return_values.include
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
